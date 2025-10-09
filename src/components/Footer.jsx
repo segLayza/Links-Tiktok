bcfd62b44e39bdcacc6e63efe6f315cba6fd6f94
segLayza <seglayza@gmail.com>
2025-10-08 19:47:02 -0500

    feat: animations

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import styleGn from '../styles/StyleGr.module.css'
 
 function Footer() {
   return (
-    <div className="footer">
-      <p>Derechos reservados © 2025 Power ♥ by Casse</p>
-         <a href="https://github.com/iamcasse">GitHub</a>
+    <div className={styleGn.footer}>
+      <p>Derechos reservados © 2025 Powered ♥ by Casse🔥</p>
+      <a href="https://github.com/iamcasse">GitHub</a>
     </div>
   );
 }
