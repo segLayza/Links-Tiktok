6853431776499164fa42d11a6bd3b1f1204c36d6
segLayza <seglayza@gmail.com>
2025-10-17 02:08:53 -0500

    feat: update

--- src/utilities/Links.js
+++ src/utilities/Links.js
@@ -2,7 +2,7 @@ let links = [
   { name: "kick", url: "https://kick.com/iamcasse" },
   { name: "youTube", url: "https://www.youtube.com/@iamcasse" },
   { name: "twitch", url: "https://www.twitch.tv/iamcasse" },
-  { name: "tiktok2", url: "https://www.tiktok.com/@iamcasse2" },
+  { name: "tiktok2", url: "https://www.tiktok.com/@iiamcasse" },
   { name: "discord", url: "https://discord.gg/MV6xjpgxMS" },
   { name: "github", url: "https://github.com/iamcasse" },
 ];
